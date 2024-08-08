915c09a625ca6e0317a52c3e0d00705390d92c71
Julio Waissman <julio.waissman@unison.mx>
2024-08-08 11:54:05 -0700

    Create hola_mundo.py

---
+++ hola_mundo.py
@@ -0,0 +1,10 @@
+import datetime
+
+def main():
+  print("Hola, mundo cruel")
+
+  time_stamp = datetime.datetime.now()
+  print("Son en estos momentos: ", time_stamp)
+
+if __name__ == "__main__":
+  main()
